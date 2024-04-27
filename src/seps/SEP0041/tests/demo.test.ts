47aef65af7236432793f6be683e10368e7e2e18e
Fabricius Zatti <62725221+fazzatti@users.noreply.github.com>
2024-04-27 18:08:18 -0300

    chore: add tslib

--- src/seps/SEP0041/tests/demo.test.ts
+++ src/seps/SEP0041/tests/demo.test.ts
@@ -1,8 +1,19 @@
+import { DefaultAccountHandlerClient } from "stellar-plus/lib/stellar-plus/account/account-handler/default";
 import configManager from "../../../core/config";
+import { Network } from "stellar-plus/lib/stellar-plus";
 
 describe("SEP0041", () => {
-  it("should pass", () => {
+  it("should pass", async () => {
     console.log(configManager.getConfig());
     expect(true).toBe(true);
+
+    const opex = new DefaultAccountHandlerClient({
+      networkConfig: Network.TestNet(),
+    });
+
+    await opex.initializeWithFriendbot();
+
+    console.log(await opex.getBalances());
+    expect(opex).toBeDefined();
   });
 });
